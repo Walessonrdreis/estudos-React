2558213a409cc31cd3aaca9094afca2950eb86e6
Walessonrdreis <wallllase@hotmail.com>
2022-05-10 21:20:56 -0300

    Inserindo aula08, completa

--- RLSystem-React/aula08/js/main.js
+++ RLSystem-React/aula08/js/main.js
@@ -1,14 +1,45 @@
-function Clock(props){
-    return (
-        <div>
-        <h1>Olá</h1>
-        <h2>São: {props.date.toLocaleTimeString()}</h2>
-    </div>
-       
-    );
+
+class Clock extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()}
+
+
+    }
+    //componente criado
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+
+    }
+    //componente removido
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+
+    }
+    //somo obrigados a usar o setState
+    tick() {
+        //se não colocar o setState ele não atualiza o render()
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render(){
+        return (
+            <div>
+            <h1>Olá</h1>
+            <h2>São: {this.state.date.toLocaleTimeString()}</h2>
+        </div>
+           
+        );
+
+    }
 }
 function tick(){
-    ReactDOM.render(<Clock date={new Date()}/>, document.getElementById('root'));
+    ReactDOM.render(<Clock/>, document.getElementById('root'));
 
 }
 
