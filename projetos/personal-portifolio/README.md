382bf4508048831549a84ad8d2070f204d94a147
Walessonrdreis <wallllase@hotmail.com>
2022-07-25 20:24:46 -0300

    configurando App.js, index.js, Context.js e App.css

--- projetos/personal-portifolio/README.md
+++ projetos/personal-portifolio/README.md
@@ -1,3 +1,6 @@
 # Projeto Personal  portfólio
 
 `npx yarn install` para instalar o yarn no projeto
+Yarn é um gerenciamento de Dependência.
+
+Configurando Context.js que terá as classes `themeReducer e ThemeProvider `para configurar o tema do site.
--- projetos/personal-portifolio/src/App.js
+++ projetos/personal-portifolio/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useContext} from "react";
+import {themeContext} from "./Context";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return(
+    <div className="App"
+    style={{ 
+      background: darkMode ? "black" : "",
+      color: darkMode ? "white" : "",
+    }}
+    >
+    <h1>Testado App</h1>
+
     </div>
-  );
+  )
 }
-
 export default App;
