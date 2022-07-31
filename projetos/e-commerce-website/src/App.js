35cfea61d15cc5b448cd2d0c61a7812028df2138
Walessonrdreis <wallllase@hotmail.com>
2022-07-30 22:13:26 -0300

    first commit

---
+++ projetos/e-commerce-website/src/App.js
@@ -0,0 +1,13 @@
+import { DataProvider } from "./Context/DataContext";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     
+    </div>
+  );
+}
+
+export default App;
